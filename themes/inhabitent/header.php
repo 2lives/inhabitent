fe7811606a0616183c2e8340ea56ab53786898df
Brandon Liu <brandon.slipjet@gmail.com>
2018-02-26 12:04:42 -0800

    fix header nav and added search svg fontawesome

--- themes/inhabitent/header.php
+++ themes/inhabitent/header.php
@@ -28,8 +28,15 @@
 
 				<nav id="site-navigation" class="main-navigation" role="navigation">
 				<a href="http://localhost:3000/project4/"><img src="<?php echo get_template_directory_uri(); ?>/Images/inhabitent-logo-tent.svg"/>
-					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html( 'Primary Menu' ); ?></button>
+					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
+					<?php esc_html( 'Primary Menu' ); ?></button>
 					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+					<div class="nav-search">
+						<a href"#" class="nav-search-toggle" aria-hidden="true">
+						<i class="fas fa-search"></i>
+						</a>
+					</div>
+
 				</nav><!-- #site-navigation -->
 			</header><!-- #masthead -->
 
