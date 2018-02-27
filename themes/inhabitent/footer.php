527880ca941e9aee4c6976bf83661aa4fd81d3a4
Brandon Liu <brandon.slipjet@gmail.com>
2018-02-26 21:48:59 -0800

    all font awesome added and styled

--- themes/inhabitent/footer.php
+++ themes/inhabitent/footer.php
@@ -12,9 +12,13 @@
 			<footer id="colophon" class="site-footer" role="contentinfo">
 				<div class="contact-info">
 					<?php echo "<h3> contact info </h3>";?>
-					<a href="<?php echo esc_url('#'); ?>"><?php printf(esc_html('info@inhabitent.com')); ?></a>
-					<a href="<?php echo esc_url('#'); ?>"><?php printf(esc_html('778-456-7891')); ?></a>
-					<!-- icons to be placed here-->
+					<p><i class="fas fa-envelope"></i><a href="<?php echo esc_url('#'); ?>"><?php printf(esc_html('info@inhabitent.com')); ?></a></p>
+					<p><i class="fas fa-phone"></i><a href="<?php echo esc_url('#'); ?>"><?php printf(esc_html('778-456-7891')); ?></a></p>
+					<p>
+						<span><i class="fab fa-facebook-square"></i></span>
+						<span><i class="fab fa-twitter-square"></i></span>
+						<span><i class="fab fa-google-plus-square"></i></span>
+					</p>
 				</div><!-- .contact-info -->
 				<div class="business-hours">
 				<?php echo "<h3> business hours </h3>";?>
